a484ac4ba6341fd2e126acd3ab3b3fd8a7fb8ce9
matias.pernasbarcelo <matias.pernasbarcelo@cognizant.com>
2024-06-07 11:22:47 +0000

    matias_notebook

--- matias.py
+++ matias.py
@@ -1,5 +1,4 @@
-import pandas as pd 
-import  numpy as np 
-
-
-print("holi")
+import pandas as pd
+import numpy as np
+data = pd.read_csv("/teamspace/studios/this_studio/Project-II/Fraud.csv")
+print(data.head())
